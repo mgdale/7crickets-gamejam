3d5eb1bf0a39c69d3f17cf54ba3dd7aeaa64c2e9
mgdale <migdalebadillacamps@gmail.com>
2026-08-09 15:27:55 -0600

    findq

--- 7-crickets/puzzles/card.gd
+++ 7-crickets/puzzles/card.gd
@@ -4,14 +4,16 @@ var card_id = 0
 var face_up = true
 var found = false
 
+var textura_dorso: Texture2D
+var textura_frente: Texture2D
+
+
 func flip(show_face_up: bool):
 	face_up = show_face_up
-	if face_up:
-		text = str(card_id)
-	else:
-		text = "?"
+	icon = textura_frente if face_up else textura_dorso
+	text = ""  # ya no mostramos texto, solo la imagen
+
 
 func _pressed():
 	if found:
 		return
-	print("Card clicked, id: ", card_id)
